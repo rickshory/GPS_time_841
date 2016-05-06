32566057504995dd0204a2cb4d55b3e4d9423f0e
Rick Shory <rickshory@gmail.com>
2016-05-05 20:58:56 -0700

    set up Timer1; not using ISR yet

--- GPS_Time_841/main.c
+++ GPS_Time_841/main.c
@@ -31,6 +31,80 @@
 
 int main(void)
 {
+	// set up a heartbeat counter, interrupt every 0.1 second
+	// set up 16-bit TIMER1
+	// F_CPU = 8MHz
+	OCR1A = 12500; // 0.1 sec; use prescaler 64
+	// only channel A will cause CTC
+
+	// note that WGM1[3:0] is split over TCCR1A and TCCR1B
+	// WGM1[3:2] is TCCR1B[4:3] while WGM1[1:0] is TCCR1A[1:0]
+
+	// TCCR1A – Timer/Counter1 Control Register A
+	// 7 COM1A1
+	// 6 COM1A0
+	// 5 COM1B1
+	// 4 COM1B0
+	// (3:2 reserved)
+	// 1 WGM11
+	// 0 WGM10
+	
+	// WGM1[3:0]=(0100), Clear Timer on Compare Match (CTC) Mode using OCR1A for TOP
+	// TCCR1B[4:3]=(0:1)
+	// TCCR1A[1:0]=(0:0)
+	// the counter is cleared to zero when the counter value (TCNT1)
+	// matches OCR1A, OCF1A Interrupt Flag Set
+	// TOV1 flag is set in the same timer clock cycle that the counter counts from MAX to 0x0000
+	// ? since timer clears on match, returns to 0, this never happens
+	
+	// in Compare Output Mode, non-PWM:
+	// do not output the compare match on any pins
+	//   COM1A[1:0], TCCR1A[7:6]=(0:0), Normal port operation, OC1A disconnected
+	//   COM0B[1:0], TCCR0A[5:4]=(0:0), Normal port operation, OC1B disconnected
+		TCCR1A = 0b00000000;
+	
+	// TCCR1B – Timer/Counter1 Control Register B
+	// 7 ICNC1: Input Capture Noise Canceler
+	// 6 ICES1: Input Capture Edge Select
+	// (5 reserved)
+	// 4 WGM13
+	// 3 WGM12
+	// 2 CS12 clock select bit 2
+	// 1 CS11 clock select bit 1
+	// 0 CS10 clock select bit 0
+	
+	// use ICNC1, ICES1 unused, leave as default of 0
+	// use WGM1[3:2]=(01) for CTC Mode
+	// use CS0[2:0]=(011), prescale 64
+	TCCR1B = 0b00001011;
+
+	// TCCR1C – Timer/Counter1 Control Register C
+	// 7 FOC1A: Force Output Compare for Channel A
+	// 6 FOC1B: Force Output Compare for Channel B
+	// (5:0 reserved)
+	// all unused, leave default of zero
+	//TCCR1C = 0;
+
+	// TIMSK1 – Timer/Counter 1 Interrupt Mask Register
+	// (7, 6, 4, 3 reserved)
+	//  When these bits are written to one, and the I-flag in the Status Register is set (interrupts globally
+	//  enabled), the corresponding interrupt is enabled. The
+	//  corresponding Interrupt Vector is executed when the
+	//  corresponding bit is set in the Timer/Counter Interrupt Flag Register – TIFR1.
+	// 5 ICIE1: Timer/Counter, Input Capture Interrupt Enable (not used here)
+	// 2 OCIE1B: Timer/Counter1 Output Compare Match B Interrupt Enable (not used here)
+	// 1 OCIE1A: Timer/Counter1 Output Compare Match A Interrupt Enable
+	// 0 TOIE1: Timer/Counter1, Overflow Interrupt Enable (not used here)
+
+	cli(); // temporarily disable interrupts
+	// set the counter to zero
+	TCNT1 = 0;
+	// enable Output Compare 1A interrupt
+	TIMSK0 |= (1<<OCIE1A);
+	// clear the interrupt flag, write a 1 to the bit location
+	TIFR1 |= (1<<OCF1A);
+	
+	// set up to blink an LED	
 	DDRA |= (1<<LED);
     while (1) 
     {	
